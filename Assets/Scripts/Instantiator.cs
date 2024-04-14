f7c608120ec6143d4b7a4c4faa82f4a564e2b69b
Alina-Alen <166939073+AlinaAleni@users.noreply.github.com>
2024-04-14 10:21:44 -0400

    Adding the ingredients

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -6,6 +6,10 @@ public class Instantiator : MonoBehaviour
 {
     public GameObject bloodyHeartPrefab;
     public GameObject rosePetalsPrefab;
+    public GameObject scorpionTailPrefab;
+    public GameObject devilsHornPrefab;
+    public GameObject graveDirtPrefab;
+    public GameObject mandrakeRootPrefab;
 
     public void InstantiateBloodyHeart()
     {
@@ -18,5 +22,29 @@ public class Instantiator : MonoBehaviour
 
         Debug.Log("Rose Petals instantiated");
         Instantiate(rosePetalsPrefab);
+    }
+    public void InstantiateScorpionTail()
+    {
+
+        Debug.Log("ScorpionTail instantiated");
+        Instantiate(scorpionTailPrefab);
+    }
+    public void InstantiateDevilsHorn()
+    {
+
+        Debug.Log("Devil's Horn instantiated");
+        Instantiate(devilsHornPrefab);
+    }
+    public void InstantiateGraveDirt()
+    {
+
+        Debug.Log("Grave dirt instantiated");
+        Instantiate(graveDirtPrefab);
+    }
+    public void InstantiateMandrakeRoot()
+    {
+
+        Debug.Log("Mandrake root instantiated");
+        Instantiate(mandrakeRootPrefab);
     }
 }
