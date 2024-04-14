dd9f3ef98482ef318252ea842309d6eac3e71fad
Alina-Alen <166939073+AlinaAleni@users.noreply.github.com>
2024-04-14 10:00:06 -0400

    Game success and failure added

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Instantiator : MonoBehaviour
 {
     public GameObject bloodyHeartPrefab;
+    public GameObject rosePetalsPrefab;
 
     public void InstantiateBloodyHeart()
     {
+        
         Debug.Log("Bloody Heart instantiated");
         Instantiate(bloodyHeartPrefab);
+    }
+    public void InstantiateRosePetals()
+    {
+
+        Debug.Log("Rose Petals instantiated");
+        Instantiate(rosePetalsPrefab);
     }
 }
