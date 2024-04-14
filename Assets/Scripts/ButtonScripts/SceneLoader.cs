e2768a17480d17ffdff4d6fd6699ed428cc4453c
Hanae Razafindrakoto <marine.raza1@gmail.com>
2024-04-14 13:24:07 -0400

    Merge branch 'DrawingThePentagram' into UI

--- Assets/Scripts/ButtonScripts/SceneLoader.cs
+++ Assets/Scripts/ButtonScripts/SceneLoader.cs
@@ -28,7 +28,8 @@ public class SceneLoader : MonoBehaviour
         if(CurrentScene.currentScene == mainSceneName)
         {
             mainSceneButton.interactable = true;
-            mainSceneUI?.SetActive(true);
+            if (mainSceneUI != null) 
+                mainSceneUI.SetActive(true);
 
         } 
     }
