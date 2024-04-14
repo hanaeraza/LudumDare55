219569029eb9f5669a0e0420604ddad2eb479ed6
Alina-Alen <166939073+AlinaAleni@users.noreply.github.com>
2024-04-13 20:26:25 -0400

    Collision check working

--- Assets/Scripts/SummonCheck.cs
+++ Assets/Scripts/SummonCheck.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class SummonCheck : MonoBehaviour
 {
     [SerializeField] GameObject StarPentagram;
+    [SerializeField] int messUps=0;
     public void checkSummon()
     {
         Debug.Log("start check");
+        Collider[] colliders = StarPentagram.GetComponentsInChildren<Collider>();
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            
+        }
     }
 }
